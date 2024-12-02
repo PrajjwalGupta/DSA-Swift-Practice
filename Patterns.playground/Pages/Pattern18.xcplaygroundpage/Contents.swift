2daa161f443635e5c9523e839bd474ab611dd9b4
PrajjwalGupta <prajjwalg786@gmail.com>
2024-12-02 18:39:17 +0530

    Pattern18completed

--- Patterns.playground/Pages/Pattern18.xcplaygroundpage/Contents.swift
+++ Patterns.playground/Pages/Pattern18.xcplaygroundpage/Contents.swift
@@ -1,13 +1,26 @@
 //: [Previous](@previous)
-
 import Foundation
+//
+//func pattern18(n: Int) {
+//    
+//    let letter = "ABCDE"
+//    for i in  1...n {
+//        for j in (1...i) {
+//
+//            print(letter[letter.index(letter.startIndex, offsetBy: j)], terminator: "")
+//        }
+//        print()
+//    }
+//}
+//pattern18(n: 5)
 
+//without error
 func pattern18(n: Int) {
-    let letter = "ABCDE"
-    for i in  1..<n {
-        for j in 1...(n-i) {
-            let sub = letter.suffix(j)
-            print(sub, terminator: " ")
+    for i in 0..<n {
+        for j in (n - i)...n {
+            let ch = Character(UnicodeScalar(65 + j)!)
+            let ch2 = Character(UnicodeScalar(64 + j)!)
+            print(ch2, terminator: " ")
         }
         print()
     }
