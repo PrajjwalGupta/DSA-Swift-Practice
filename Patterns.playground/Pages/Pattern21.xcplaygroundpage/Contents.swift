a706906928fe26f8f9d7095fd51be23d7f95aa4e
PrajjwalGupta <prajjwalg786@gmail.com>
2024-12-03 10:51:05 +0530

    patterns solved

--- Patterns.playground/Pages/Pattern21.xcplaygroundpage/Contents.swift
+++ Patterns.playground/Pages/Pattern21.xcplaygroundpage/Contents.swift
@@ -2,6 +2,16 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+func pattern21(n: Int) {
+    for i in 1...n {
+        for j in 1...n {
+            if  j == 1 || i == 1 || i == n || j == n{
+                print("* ", terminator: "")
+            } else  {
+                print(" ", terminator: " ")
+            }
+        }
+        print()
+    }
+}
+pattern21(n: 10)
--- Patterns.playground/Pages/Pattern22.xcplaygroundpage/Contents.swift
+++ Patterns.playground/Pages/Pattern22.xcplaygroundpage/Contents.swift
@@ -1,7 +1,16 @@
-//: [Previous](@previous)
-
 import Foundation
 
-var greeting = "Hello, playground"
-
-//: [Next](@next)
+func pattern22(n: Int) {
+    for i in (0..<(2*n-1)) {
+        for j in (0..<(2*n-1)) {
+            var top = i
+            var bottom = j
+            var right = (2*n-2) - j
+            var left = (2*n-2) - i
+            print("\(n - min(min(top, bottom), min(right, left))) ", terminator: "")
+        }
+        print()
+    }
+             
+}
+pattern22(n: 4)
