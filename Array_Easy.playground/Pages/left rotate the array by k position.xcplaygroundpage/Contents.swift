02bb1340c4f7b1d1c5aff67b6e65d52dbf90189c
PrajjwalGupta <prajjwalg786@gmail.com>
2024-12-12 15:02:59 +0530

    linear search

---
+++ Array_Easy.playground/Pages/left rotate the array by k position.xcplaygroundpage/Contents.swift
@@ -0,0 +1,7 @@
+//: [Previous](@previous)
+
+import Foundation
+//Given an array of integers, rotating array of elements by k elements either left or right.
+
+
+//: [Next](@next)
---
+++ Array_Easy.playground/Pages/left rotate the array by one place.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//Input:
+// N = 5, array[] = {1,2,3,4,5}
+//Output:
+// 2,3,4,5,1
+
+//: [Next](@next)
